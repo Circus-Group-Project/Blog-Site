05d14bb1dc4d1fd46887b3c1ecbbcafa5557c864
Nirmith Victor <nirmithdalmeida@hotmail.com>
2023-06-07 18:31:03 -0400

    Added UI Components
    About and Contact Pages are completed

--- blogsite/src/components/about/About.js
+++ blogsite/src/components/about/About.js
@@ -1,18 +1,67 @@
 import React from 'react'
 import './About.css'
+import dev1Image from '../../assets/dev1pfp.png'
+import dev2Image from '../../assets/dev2pfp.png'
+import { FiGithub, FiInstagram, FiLinkedin} from 'react-icons/fi'
+
 const About = () => {
   return (
-    <div className='hero'>
-        <div className='container'>
-            <div className='content'>
-                <h1 className='text'>WELCOME TO OUR BLOG <span className='site'>SITE</span></h1>
-                <div className='aboutBtn'>
-                  <button>About Us</button>
+    <div className='about'>
+      <div className='content'>
+        <div className="heading">
+          <h2>About Us</h2>
+        </div>
+        <div className='contentText'>
+          <p className='para'>This is a website project to commemorate the formation of our weird little friend cicle which had humble beginnings in League of Legends
+            and from the start of our university.  We are a bunch of <span>4th year</span> and soon to be <span>Software Engineers </span>who thought this would be a good way
+            to learn AGILE, team work and web development principles.<br/>
+            Apart from this! We are just a bunch of chill gamers who are in our own league of tilt.
+            <br/>
+            We welcome you to our humble website! <br/>Feel free to connect with the developers responsible for this project!
+
+          </p>
+
+        </div>
+      </div>
+      <div className='devContainers'>
+        <h2>Developers</h2>
+        <div className='containers'>
+            {/*dev 1 */}
+            <div className='dev1'>
+                <div className='devContent'>
+                    <div className='devImage'>
+                        <div>
+                        <img src={dev1Image} alt='dev 1' style={{width: '100%', height: '100%', borderRadius:'50%'}}></img>
+                        </div>
+                    </div>
+                    <div className='devName'>Nirmith V. D'Almeida</div>
+                    <div className='socials'>
+                        <a href="https://www.instagram.com/not_jay_codes_/" target="_blank" rel="noopener noreferrer"><FiInstagram className='social-icon'/></a>
+                        <a href="https://www.linkedin.com/in/nirmith-dalmeida-81ab2418a/" target="_blank" rel="noopener noreferrer"><FiLinkedin className='social-icon'/></a>
+                        <a href="https://github.com/NirmithDev" target="_blank" rel="noopener noreferrer"><FiGithub className='social-icon'/></a>
+                    </div>
+                </div>
+            </div>
+            {/*Dev 2 */}
+            <div className='dev1'>
+                <div className='devContent'>
+                    <div className='devImage'>
+                        <div>
+                        <img src={dev2Image} alt='dev 1' style={{width: '100%', height: '100%', borderRadius:'50%'}}></img>
+                        </div>
+                    </div>
+                    <div className='devName'>Abhai R. Gill</div>
+                    <div className='socials'>
+                        <a href="https://www.instagram.com/__roop__g/" target="_blank" rel="noopener noreferrer"><FiInstagram className='social-icon'/></a>
+                        <a href="https://www.linkedin.com/in/abhai-g/" target="_blank" rel="noopener noreferrer"><FiLinkedin className='social-icon'/></a>
+                        <a href="https://github.com/abhai28" target="_blank" rel="noopener noreferrer"><FiGithub className='social-icon'/></a>
+                    </div>
                 </div>
             </div>
         </div>
+      </div>
     </div>
   )
-}
+};
 
 export default About
--- blogsite/src/components/contact/Contact.js
+++ blogsite/src/components/contact/Contact.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 import './Contact.css'
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_o5r4fdj', 'template_n87tw4l', form.current, '7-iJou_mtFeypOg1K')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+    };
   return (
-    <div className='hero'>
-        <div className='container'>
-            <div className='content'>
-                <h1 className='text'>WELCOME TO OUR BLOG <span className='site'>SITE</span></h1>
-                <div className='aboutBtn'>
-                  <button>About Us</button>
+    <div className='contact'>
+            <div className="container">
+                <div className="form-container">
+                    <form ref={form} onSubmit={sendEmail}>
+                        <h1><span>Contact</span> Us</h1>
+                        <div>
+                            <label>Name</label>
+                            <input type="text" name="user_name" placeholder='Enter your name' required/>
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input type="email" name="user_email" placeholder='Enter your email' required/>
+                        </div>
+                        <div>
+                            <label>Message</label>
+                            <textarea rows='10' name="message" placeholder='Enter your message'/>
+                        </div>
+                        <button>Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
   )
 }
 
