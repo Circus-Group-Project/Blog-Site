c4ddadb56ae6a00825e890f0123644d97863130e
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2023-06-19 21:55:57 -0400

    Example for Communicating with database

--- blogsite/src/App.js
+++ blogsite/src/App.js
@@ -1,11 +1,24 @@
-import Home from './components/home/home'
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer'
+import React from "react";
+import api from "./api/axiosConfig";
+import Home from "./components/home/home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+
 function App() {
+  const getGallery = async () => {
+    try {
+      const response = await api.get("/api/v1/gallery");
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  getGallery();
+
   return (
     <>
       <Navbar></Navbar>
-      <Home/>
+      <Home />
       <Footer></Footer>
     </>
   );
