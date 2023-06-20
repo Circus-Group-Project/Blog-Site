fbd2f09de6f15bee727314dd253e8d357e7cbcfc
Nirmith Victor <nirmithdalmeida@hotmail.com>
2023-06-20 16:33:04 -0400

    added functionality to gallery page

--- blogsite/src/App.js
+++ blogsite/src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
-import { useEffect,useState } from "react";
-import api from "./api/axiosConfig";
 import Home from "./components/home/home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
 function App() {
-  const getGallery = async () => {
-    try {
-      const response = await api.get("/api/v1/gallery");
-      console.log(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  getGallery();
 
   return (
     <>
