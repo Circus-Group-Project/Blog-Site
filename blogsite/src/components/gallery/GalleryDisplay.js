8ed821566330d0566d42519ff300be1197f4fed8
Nirmith Victor <nirmithdalmeida@hotmail.com>
2023-06-21 13:37:24 -0400

    added display for each gallery

--- blogsite/src/components/gallery/GalleryDisplay.js
+++ blogsite/src/components/gallery/GalleryDisplay.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect,useState } from "react";
 import api from "../../api/axiosConfig";
-
+import './GalleryDisplay.css';
 
 const GalleryDisplay = () => {
   const { id } = useParams();
@@ -19,15 +18,44 @@ const GalleryDisplay = () => {
       }
     };
     getGallery();
-  },[]);
+  },[id]);
 
   return (
-    <div>
-      <h1>Gallery Page</h1>
-      <p>Gallery ID: {id}</p>
-      {/* Additional content for the gallery page */}
+    <div className='galleryDisplay'>
+      {data && data.pictures && (
+        <div className='dataContainer'>
+          <h2>{data.name}</h2>
+          <div className='contents'>
+            <p><span>Tag</span>: {data.tag}</p>
+            <h2>Pictures</h2>
+            <div className='pictures'>
+              <div className='containers'>
+              {data.pictures.map((picture, index) => {
+                const a = picture.link.replace('https://drive.google.com/file/d/', 'https://drive.google.com/uc?id=');
+                const mainImage = a.replace('/view', '');
+                console.log(a)
+                console.log(mainImage)
+                return (
+                  <a href={mainImage} target="_blank" rel="noopener noreferrer" key={picture.pictureID}>
+                    <div className='img1Container'>
+                      <div className='devImage'>
+                          <div>
+                          <img src={mainImage} alt={picture.id.pictureID} style={{height: '100%', borderRadius:'10%'}}></img>
+                          </div>
+                      </div>
+                      <div className='devName'>Picture {index+1}</div>
+                    </div>
+                  </a>
+                );
+              })}
+              </div>
+            </div>
+          </div>
+        </div>
+        )}      
     </div>
   )
 }
 
 export default GalleryDisplay
+//<p>Gallery ID: {id}</p>
